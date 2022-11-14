7388594e5dfdb0fea0cf510c4927d2e92248efda
bell-ho <jj0101065@gmail.com>
2022-11-14 10:46:03 +0900

    hashtag 페이지

--- prepare/front/pages/hashtag/[tag].js
+++ prepare/front/pages/hashtag/[tag].js
@@ -1,82 +1,85 @@
 import React, { useEffect } from 'react';
 import AppLayout from '~/components/AppLayout';
-import { useDispatch, useSelector } from 'react-redux';
 import PostCard from '~/components/PostCard';
-import { LOAD_HASHTAG_POSTS_REQUEST } from '~/reducers/post';
-import { LOAD_MY_INFO_REQUEST } from '~/reducers/user';
-import wrapper from '~/store/configureStore';
-import { END } from 'redux-saga';
-import axios from 'axios';
 import { useRouter } from 'next/router';
+import { useInView } from 'react-intersection-observer';
+import { dehydrate, QueryClient, useInfiniteQuery } from 'react-query';
+import { queryKeys } from '~/react_query/constants';
+import { loadHashtagPostsAPI } from '~/api/posts';
 
 const Hashtag = () => {
-  const dispatch = useDispatch();
-
+  const [ref, inView] = useInView();
   const router = useRouter();
   const { tag } = router.query;
 
-  const { mainPosts, hasMorePosts, loadPostsLoading } = useSelector(
-    (state) => state.post,
+  const {
+    data,
+    isLoading: loadPostsLoading,
+    fetchNextPage,
+  } = useInfiniteQuery(
+    [queryKeys.hashtag, tag],
+    ({ pageParam = '' }) => loadHashtagPostsAPI(tag, pageParam),
+    {
+      getNextPageParam: (lastPage) => {
+        return lastPage?.[lastPage.length - 1]?.id;
+      },
+    },
   );
 
-  useEffect(() => {
-    function onScroll() {
-      if (
-        window.scrollY + document.documentElement.clientHeight >
-        document.documentElement.scrollHeight - 300
-      ) {
-        if (hasMorePosts && !loadPostsLoading) {
-          const lastId = mainPosts[mainPosts.length - 1]?.id;
-          dispatch({
-            type: LOAD_HASHTAG_POSTS_REQUEST,
-            lastId: lastId,
-            data: tag,
-          });
-        }
-      }
-    }
+  const mainPosts = data?.pages.flat();
+  const isEmpty = data?.pages[0]?.length === 0;
+  const isReachingEnd =
+    isEmpty || (data && data.pages[data.pages.length - 1]?.length < 10);
+  const hasMorePosts = !isEmpty && !isReachingEnd;
+  const readToLoad = hasMorePosts && !loadPostsLoading;
 
-    window.addEventListener('scroll', onScroll);
-    return () => {
-      window.removeEventListener('scroll', onScroll);
-    };
-  }, [hasMorePosts, loadPostsLoading, mainPosts, tag]);
+  useEffect(() => {
+    if (inView && readToLoad) {
+      fetchNextPage();
+    }
+  }, [inView, readToLoad, fetchNextPage]);
 
   return (
     <AppLayout>
       {mainPosts.map((c) => (
         <PostCard key={c.id} post={c} />
       ))}
+      <div
+        ref={readToLoad ? ref : undefined}
+        style={{ height: 50, backgroundColor: 'yellow' }}
+      />
     </AppLayout>
   );
 };
 
-// 온전히 프론트 서버에서 실행됨
-export const getServerSideProps = wrapper.getServerSideProps(
-  async (context) => {
-    //context 안에 store가 있음
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
 
-    const cookie = context.req ? context.req.headers.cookie : '';
-    axios.defaults.headers.Cookie = '';
+export const getStaticProps = async (context) => {
+  const queryClient = new QueryClient();
+  const tag = context.params?.tag;
+  if (!tag) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: true,
+      },
+    };
+  }
 
-    //다른 사람과 쿠키 공유 문제 해결
-    if (context.req && cookie) {
-      // 프론트 서버일때와 쿠키가 있을때만 쿠키를 넣는다
-      axios.defaults.headers.Cookie = cookie;
-    }
+  await queryClient.prefetchInfiniteQuery([queryKeys.hashtag, tag], () =>
+    loadHashtagPostsAPI(tag),
+  );
 
-    context.store.dispatch({
-      type: LOAD_MY_INFO_REQUEST,
-    });
-
-    context.store.dispatch({
-      type: LOAD_HASHTAG_POSTS_REQUEST,
-      data: context.params.tag,
-    });
-
-    context.store.dispatch(END);
-    await context.store.sagaTask.toPromise();
-  },
-);
+  return {
+    props: {
+      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+    },
+  };
+};
 
 export default Hashtag;
